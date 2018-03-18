71744a11672ae7b4cc49a063b6b17e1736a1730d
trahoa <32536642+trahoa@users.noreply.github.com>
2018-03-18 07:43:30 +0700

    Update install-opencv.sh

--- install-opencv.sh
+++ install-opencv.sh
@@ -8,18 +8,18 @@ sudo apt-get install -y libcanberra-gtk*
 sudo apt-get install -y libatlas-base-dev gfortran
 sudo apt-get install -y python2.7-dev python3-dev
 
-cd ~
-wget -O opencv.zip https://github.com/opencv/opencv/archive/3.3.0.zip
-unzip opencv.zip
-wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/3.3.0.zip
-unzip opencv_contrib.zip
+cd ~/workspace/
+mkdir opencv
+curl -L https://github.com/opencv/opencv/archive/3.3.0.tar.gz | tar xz -C opencv --strip-components 1
+mkdir opencv_contrib
+curl -L https://github.com/opencv/opencv_contrib/archive/3.3.0.tar.gz | tar xz -C opencv_contrib --strip-components 1
 
-cd ~/opencv-3.3.0/
+cd ~/workspace/opencv/
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
  -D CMAKE_INSTALL_PREFIX=/usr/local \
- -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.3.0/modules \
+ -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
  -D ENABLE_NEON=ON \
  -D ENABLE_VFPV3=ON \
  -D BUILD_TESTS=OFF \
@@ -27,10 +27,11 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
  -D BUILD_EXAMPLES=OFF ..
 make -j4
 sudo make install
-sudo ldconfig
 #cd /usr/local/lib/python3.5/site-packages/
 #sudo mv cv2.cpython-35m-arm-linux-gnueabihf.so cv2.so
 
 # Tensorflow install
 wget https://github.com/lhelontra/tensorflow-on-arm/releases/download/v1.4.0/tensorflow-1.4.0-cp35-none-linux_armv7l.whl
 sudo pip3 install tensorflow-1.4.0-cp35-none-linux_armv7l.whl
+rm tensorflow-1.4.0-cp35-none-linux_armv7l.whl
+sudo ldconfig
